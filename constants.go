570e59eba59a544d2c886427a8f1abee6cae9bea
Bharath-reddy-eng <108502451+Bharath-reddy-eng@users.noreply.github.com>
2023-03-18 22:50:33 -0700

    Create constants.go

---
+++ constants.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const (
+	Pi    = 3.14
+	World = "世界"
+	Truth = true
+)
+
+func main() {
+	fmt.Println("Hi", World)
+	fmt.Println("I am ", Pi, "Kg")
+	fmt.Println("Its", Truth)
+}
